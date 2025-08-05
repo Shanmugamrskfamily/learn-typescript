4b86d0e36ed4118d2c0965017afda57db699a375
SHANMUGAM R <shanmugamrskfamily@gmail.com>
2025-08-05 23:11:23 +0530

    Basic Setup

---
+++ Day01/increment-decrement/pages/index.tsx
@@ -0,0 +1,106 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import { useState } from "react";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+  const handleCountChange = ({ countType }:{countType:string}) => {
+    if (countType == "increment") {
+      setCount((prev) => prev + 1);
+    } else if (countType == "decrement") {
+      setCount((prev) => prev - 1);
+    }
+  };
+  return (
+    <div
+      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+    >
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+
+        <div className="flex justify-center gap-1 text-white text-sm">
+          <p className="">Current Count:</p>
+          <p>{count}</p>
+        </div>
+        <div className="flex items-center gap-8 text-sm">
+          <button
+            onClick={() => handleCountChange({ countType: "decrement" })}
+            className="px-4 py-2 border-[1px] border-red-500 text-white cursor-pointer rounded-sm transition-all duration-300 ease-in-out hover:shadow-sm  hover:shadow-red-500"
+          >
+            Decrease
+          </button>
+          <button
+            onClick={() => handleCountChange({ countType: "increment" })}
+            className="px-4 py-2 border-[1px] border-green-500 text-white cursor-pointer rounded-sm transition-all duration-300 ease-in-out hover:shadow-sm  hover:shadow-green-500"
+          >
+            Increase
+          </button>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
